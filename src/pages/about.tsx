d5d289d3d7558f326bfe3bc3737665d64948c899
Arunnya HomeLab <arunnya.varma.india@gmail.com>
2024-07-08 20:22:31 +0530

    ok

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -102,22 +102,20 @@ const About = () => {
         </div>
       </section> */}
 
-      <section data-aos="fade-left" data-aos-delay="300" className="text-gray-600 body-font">
-  <div className="container px-5 py-12 mx-auto flex flex-wrap">
-    <h2 className="sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5">Kickstarter Actually Pinterest Brunch Bitters Occupy</h2>
-    <div className="md:w-3/5 md:pl-6">
-      <p className="leading-relaxed text-base">Taxidermy bushwick celiac master cleanse microdosing seitan. Fashion axe four dollar toast truffaut, direct trade kombucha brunch williamsburg keffiyeh gastropub tousled squid meh taiyaki drinking vinegar tacos.</p>
-      <div className="flex md:mt-4 mt-6">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
-        <a className="text-indigo-500 inline-flex items-center ml-4">Learn More
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </a>
-      </div>
-    </div>
-  </div>
-</section>
+      <section style={{
+        backgroundImage: "url('https://a.storyblok.com/f/90078/ade9a4f8e6/2020-11_visionarticle.png')",
+        backgroundRepeat: 'no-repeat'
+      }} data-aos="fade-left" data-aos-delay="300" className="text-gray-600 body-font border-t-2 border-b-2">
+        <div className="container px-5 py-12 mx-auto flex flex-wrap items-center">
+          <h2 className={classNames("sm:text-3xl text-2xl text-gray-900 font-medium title-font mb-2 md:w-2/5", InterBold.className)}>Our Vision</h2>
+          <div className="md:w-3/5 md:pl-6">
+            <p className="leading-relaxed text-base">
+              To be the global leader in manufacturing, renowned for our
+              creativity and innovations.
+            </p>
+          </div>
+        </div>
+      </section>
 
       <section data-aos="fade-down" data-aos-delay="400" className="text-gray-600 body-font">
         <div className="container px-5 py-12 mx-auto">
