01533e2fea8ff5d123f47f8600eae136d07038be
Arunnya HomeLab <arunnya.varma.india@gmail.com>
2024-07-09 20:26:23 +0530

    ok

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -461,11 +461,16 @@ const Home = () => {
 
         <div data-aos="fade-down" data-aos-delay="200" className="bg-[#d3d3d3] relative flex items-center justify-center overflow-hidden ">
           <div className="relative mx-auto h-full sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
-            <img src='/nalini copy.jpg' />
+            <img src='/nalini.jpg' />
+          </div>
+        </div>
+        <div data-aos="fade-left" data-aos-delay="200" className="bg-[#E8E1DE] relative flex items-center justify-center overflow-hidden ">
+          <div className="relative mx-auto h-full sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+            <img src='/samir.jpg' />
           </div>
         </div>
         <div data-aos="fade-left" data-aos-delay="200" className="bg-white relative flex items-center  n justify-center overflow-hidden ">
-          <div className="relative mx-auto h-full px-4  pb-20   md:pb-10 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+          {/* <div className="relative mx-auto h-full px-4  pb-20   md:pb-10 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
             <div className="flex flex-col items-center justify-between lg:flex-row py-16">
               <div className="relative hidden lg:mr-32 lg:block lg:w-1/2">
                 <svg xmlns="http://www.w3.org/2000/svg" className="my-6 mx-auto h-10 w-10 animate-bounce rounded-full bg-white p-2 lg:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -493,7 +498,7 @@ const Home = () => {
               </div>
 
             </div>
-          </div>
+          </div> */}
           <div className="hidden text-9xl varien absolute top-6 left-1/4 text-g/10 z-40    ">
             About Us
           </div>
